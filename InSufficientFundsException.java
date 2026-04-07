9e0e6195b376a28cabf0a0d230722948ab60ce9c
AbinayaVJ <139043200+AbinayaVJ@users.noreply.github.com>
2026-04-07 12:51:00 +0530

    InSufficientFundsException.java

--- InSufficientFundsException.java
+++ InSufficientFundsException.java
@@ -7,25 +7,5 @@ class InSufficientFundsException extends Exception {
     }
 }
 
-/*class InSufficientFundsException extends Exception {
-    /**
-	 * 
-	 */
-	/*private static final long serialVersionUID = 1L;
-
-	public InSufficientFundsException(String message) {
-        super(message);
-    }
-}*/
 
 
-/*public BankingSystem() {
-// TODO Auto-generated constructor stub
-}
-
-public static void main(String[] args) {
-// TODO Auto-generated method stub
-
-}
-
-}*/
